5a03a8efc26f48c26ae0db6e8e3d450c9d52febb
John Howard <longhornjoho@gmail.com>
2016-05-08 03:37:27 +0000

    setup of sendgrid for email notifications

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'dry-atoll-25656.herokuapp.com' }
 end
