5453dc9d77a40e0bb37604b7b3375b936e9602e9
John Howard <longhornjoho@gmail.com>
2016-05-14 21:29:38 +0000

    added users controller and index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :authenticate_user!
+  
+  def after_sign_in_path_for(resource)
+    users_index_path
+  end
+  
   protected
 
   def configure_permitted_parameters
